23292a115c6ace53623edb62ffa0f23c8f694848
Lauren Pease <lauren@laurenpease.com>
2019-11-16 08:27:20 -0800

    refresh

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-
-if (typeof window !== 'undefined') {
-    document.addEventListener('DOMContentLoaded', function(){
-        ReactDOM.render(<BrowserRouter><Hello /></BrowserRouter>, document.getElementById('app'))
-    })
-}
