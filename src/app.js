ec629a7fc59d2eff802375d983f055af1023d4a5
Lauren Pease <lauren@laurenpease.com>
2019-11-16 08:25:00 -0800

    clean

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const App = React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt an Octopus")
+);
+
+ReactDOM.render(
+    App,
+    document.getElementById('root')
+);
