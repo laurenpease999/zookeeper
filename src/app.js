c9f85911605e8b3285767fbe3b2d8ac776b91715
Lauren Pease <lauren.pease@officeally.com>
2019-11-21 08:22:54 -0800

    pkg

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const App = React.createElement(
     "div",
-    {},
-    React.createElement("h1", {}, "Zookeeper")
+    {id: "title"},
+    React.createElement("h1", {}, "Available for Adoption"),
+    React.createElement(Animal, {
+        name: "Jasper",
+        type: "Red Panda",
+        age: "Adult"
+    })
 );
+
+/*
+Name
+Type
+Age - Young, Adult, Senior 
+*/ 
+const Animal = ({ name, type, age }) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, type),
+        React.createElement("h2", {}, age)
+    ]);
+};
 
 ReactDOM.render(
     App,
