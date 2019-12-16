4d61f24f67c4322183388d797530ef51c81413ee
Lauren Pease <lauren@laurenpease.com>
2019-12-15 19:02:52 -0800

    params state

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Animal from "./components/animal"
+import SearchParams from './components/SearchParams.js'; 
 
 const App = () => {
     return (
         <div>
             <h1>Available for Adoption</h1>
-            <Animal name="Jasper" type="Red Panda" age="Adult" />
-            <Animal name="Lily" type="Monkey" age="Young" />
-            <Animal name="George" type="Chimp" age="Senior" />
+            <SearchParams />
         </div>
     );
 }; 
