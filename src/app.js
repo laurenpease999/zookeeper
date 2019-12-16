57fffdef20d96848cd1cf13b639fb3e26b0bb5f3
Lauren Pease <lauren@laurenpease.com>
2019-12-15 16:02:44 -0800

    bundle

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-const App = React.createElement(
-    "div",
-    {id: "title"},
-    React.createElement("h1", {}, "Available for Adoption"),
-    React.createElement(Animal, {
-        name: "Jasper",
-        type: "Red Panda",
-        age: "Adult"
-    })
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
 /*
 Name
@@ -20,9 +12,32 @@ const Animal = ({ name, type, age }) => {
         React.createElement("h2", {}, type),
         React.createElement("h2", {}, age)
     ]);
-};
+}; 
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {id: "title"}, [
+        React.createElement("h1", {}, "Available for Adoption"),
+        React.createElement(Animal, {
+            name: "Jasper",
+            type: "Red Panda",
+            age: "Adult"
+        }), 
+        React.createElement(Animal, {
+            name: "Lily",
+            type: "Monkey",
+            age: "Young"
+        }),
+        React.createElement(Animal, {
+            name: "George",
+            type: "Chimp",
+            age: "Senior"
+        })       
+    ]);
+}; 
 
 ReactDOM.render(
-    App,
+    React.createElement(App),
     document.getElementById('root')
 );
