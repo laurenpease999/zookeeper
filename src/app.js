6b6fdcd8af4567eb2ccddbf1c3c68522233f89fa
Lauren Pease <lauren.pease@officeally.com>
2020-02-17 14:26:36 -0800

    router

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Link } from '@reach/router';
 import SearchParams from './components/SearchParams.js'; 
+import Details from './components/Details.js'; 
 
 const App = () => {
     return (
         <div>
             <h1>Available for Adoption</h1>
-            <SearchParams />
+            <header>
+                <Link to="/">Adopt Me</Link>
+            </header>
+            <Router>
+                <SearchParams path="/" />
+                <Details path="/details/:id" />
+            </Router>
         </div>
     );
 }; 
---
+++ src/components/Details.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Details = props => {
+    return <h1>details</h1>
+};
+
+export default Details;
+
